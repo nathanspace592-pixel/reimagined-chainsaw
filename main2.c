ae2eb04a3559a6038d2be5e467c5cb49c28278cb
nathanspace592-pixel <nathanspace592@gmail.com>
2025-12-11 21:16:56 +0100

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+
+int main(){
+
+//   //creating a variable.
+  char letters = "a";
+
+//   //Declaring a variable
+  char letter = " \ " ;
+
+
+
+ int number1 = 5;
+ int number2 = 9;
+
+ int sum = number1 + number2;
+
+ printf("%d", sum);
+
+
+//  int number1 = 5;
+//  int number2 = 7;
+//  int sum = number1 + number2;
+//  float divide = number1 / number2;
+//  int multiply = number1 * number2;
+//  int minus  = number2 - number1;
+//     printf("The sum of 5 + 7:  %d\n", &sum );
+//     printf("The division of 5 / 7:\n ");
+//     printf("%f\n", &divide );
+//     printf("The multiplication of 5 * 7:\n ");
+//     printf("%d\n", &multiply);
+//     printf("The subtraction of 7 - 5:\n ");
+//     printf("%d\n", &minus);
+
+//     printf("%d","%f","%d","%d", sum, multiply, divide, minus);
+//        printf("%d",sum);
+
+//     return 0;
+
+
+
+//  int length = 15;
+//  int width = 14;
+//  int  area = length * width;
+//  int perimeter = 2*length + 2*width;
+//    printf("The area of the rectangle is: %d\n", area);
+//    printf("The  perimeter of the rectangle is: %d\n", perimeter);
+//    return 0;
+//   printf("Enter a number: ");
+//   scanf("%d", number);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
